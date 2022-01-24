6fe20dd92bb73b8977b7687b33fa601904412d7b
harissucipto <harisssucipto@gmail.com>
2022-01-24 13:42:47 +0800

    add vote as test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Jumbotron from "./chapter-2/Jumbotron";
 // import Table from './chapter-2/Table'
 // import fakeEmployees from './chapter-2/mocks/employees'
-import Vote from './chapter-3/Vote'
+// import Vote from './chapter-3/Vote'
+
+import EmployeeEmail from './chapter-3/EmployeeEmail'
 
 const App = () => {
   return (
     <>
-      <Vote totalGlobalLikes={10} />
+      <EmployeeEmail />
     </>
   )
 }
