730ae77912b726a00c98630332a98880c85b47d3
harissucipto <harisssucipto@gmail.com>
2022-01-24 12:10:43 +0800

    add test learn how to test presentational component

--- src/chapter-2/Table.test.js
+++ src/chapter-2/Table.test.js
@@ -2,10 +2,21 @@ import { render, screen } from '@testing-library/react'
 import fakeEmployees from './mocks/employees'
 import Table from './Table'
 
+// learn how to test presentational components
+
 it('renders with expected values', () => {
   render(<Table employees={fakeEmployees} />)
 
   expect(screen.getByRole('cell', { name: /john smith/i })).toBeInTheDocument()
   expect(screen.getByRole('cell', { name: /engineering/i })).toBeInTheDocument()
   expect(screen.getByRole('cell', { name: /designer/i })).toBeInTheDocument()
+})
+
+it('has the correct class', () => {
+  render(<Table employees={fakeEmployees} />)
+
+  expect(screen.getByRole('table')).toHaveAttribute(
+    'class',
+    'table table-striped'
+  )
 })
