029a627a305b88432a940b5d2b0fecc3ba32a26c
harissucipto <harisssucipto@gmail.com>
2022-01-24 11:52:00 +0800

    add travel test use case

---
+++ notes.md
@@ -0,0 +1,4 @@
+## Presentational Component -> are the components that do not manage state
+
+## snapshot testing -> use when you want make sure the html output does not change unexpectedly
+
---
+++ src/chapter-2/Travel.js
@@ -0,0 +1,16 @@
+const Travel = () => {
+  return (
+    <div className="card text-center">
+      <i className="material-icons" style={{ fontSize: '4rem' }}>
+        airplanemode_active
+      </i>
+      <h4>Travel Anywhere</h4>
+      <p className="p-1">
+        Our premium package allows you to take exotic trips anywhere at the
+        cheapest prices!
+      </p>
+    </div>
+  )
+}
+
+export default Travel
